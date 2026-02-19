200e4b573e47b5565a4acc1f0caacc418b709dd1
n3okyshi <n3okyshi@gmail.com>
2026-02-19 05:41:41 -0300

    updating

---
+++ js/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: atob("QUl6YVN5REJZOWhERVR1Z3pVYWNXcm1mcUgwNm9CTlpmR0FIXzJz"),
+    authDomain: "planner-9aeac.firebaseapp.com",
+    projectId: "planner-9aeac",
+    storageBucket: "planner-9aeac.firebasestorage.app",
+    messagingSenderId: "196600313427",
+    appId: "1:196600313427:web:8a8e76842163021d48b8a6"
+};
